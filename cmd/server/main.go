95ceb63b36289d1e37bb934a49ba30ae38fca947
Galbeyte <s.galbeyte@gmail.com>
2023-08-15 00:48:13 -0400

    Using gorilla mux for routing.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,12 +2,29 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
+	transportHTTP "github.com/Galbeyte1/go-cars-service/api/http"
+	"github.com/Galbeyte1/go-cars-service/internal/car"
+	"github.com/Galbeyte1/go-cars-service/internal/database"
 )
 
 type App struct {}
 
 func (app *App) Run() error {
 	fmt.Println("Running Application...")
+
+	db := database.NewDatabase()
+
+	carService := car.NewService(db)
+
+	handler := transportHTTP.NewHandler(carService)
+	handler.SetupRoutes()
+
+	if err := http.ListenAndServe(":8080", handler.Router); err != nil {
+		fmt.Println("Failed to set up server")
+		return err
+	}
 
 	return nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Galbeyte1/go-cars-service
 
 go 1.20
+
+require github.com/gorilla/mux v1.8.0
