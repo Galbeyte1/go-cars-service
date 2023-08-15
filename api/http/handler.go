69087b626ee09791f7524e40b87ecd63deb0ced9
Galbeyte <s.galbeyte@gmail.com>
2023-08-15 02:57:30 -0400

    Using service methods in routed http handler methods to fulfil endpoint requirments.

--- api/http/handler.go
+++ api/http/handler.go
@@ -52,21 +52,75 @@ func (h *Handler) SetupRoutes() {
 // GetCar - retrieve a car by ID
 func (h *Handler) GetCar(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("GetCar Endpoint Hit!")
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	car, err := h.Service.GetCar(id)
+	if err != nil {
+		sendErrorResponse(w, "Failed to retrieve car by car ID", err)
+		return 
+	}
+
+	if err := sendOkResponse(w, car); err != nil {
+		panic(err)
+	}
 }
 
 // GetAllCars - retrieve all cars
 func (h *Handler) GetAllCars(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("GetAllCars Endpoint Hit!")
+
+	cars, err := h.Service.GetAllCars()
+	if err != nil {
+		sendErrorResponse(w, "Failed to retrieve all cars", err)
+		return
+	}
+
+	if err := sendOkResponse(w, cars); err != nil {
+		panic(err)
+	}
 }
 
 // PostCar - add a new car
 func (h *Handler) PostCar(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("PostCar Endpoint Hit!")
+	var car car.Car
+	if err := json.NewDecoder(r.Body).Decode(&car); err != nil {
+		sendErrorResponse(w, "Failed to decode JSON body", err)
+		return
+	}
+
+	car, err := h.Service.PostCar(car)
+
+	if err != nil {
+		sendErrorResponse(w, "Failed to post new car", err)
+		return
+	}
+	if err := sendOkResponse(w, car); err != nil {
+		panic(err)
+	}
 }
 
 // UpdateCar - updates a car by ID
 func (h *Handler) UpdateCar(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("UpdateCar Endpoint Hit!")
+	var car car.Car
+	if err := json.NewDecoder(r.Body).Decode(&car); err != nil {
+		sendErrorResponse(w, "Failed to decode JSON body", err)
+		return
+	}
+
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	car, err := h.Service.UpdateCar(id, car)
+	if err != nil {
+		sendErrorResponse(w, "Failed to update car", err)
+		return
+	}
+	if err := sendOkResponse(w, car); err != nil {
+		panic(err)
+	}
 }
 
 
@@ -77,4 +131,12 @@ func sendOkResponse(w http.ResponseWriter, resp interface{}) error {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
 	return json.NewEncoder(w).Encode(resp)
+}
+
+func sendErrorResponse(w http.ResponseWriter, message string, err error) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusInternalServerError)
+	if err := json.NewEncoder(w).Encode(Response{Message: message, Error: err.Error()}); err != nil {
+		panic(err)
+	}
 }
